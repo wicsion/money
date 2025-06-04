2eb9a3dd5eff188b87805877ddefcd6c6be7da68
wicsion <goldin301@gmail.com>
2025-06-05 02:41:40 +0300

    c

--- real_estate_portal/settings.py
+++ real_estate_portal/settings.py
@@ -99,11 +99,11 @@ WSGI_APPLICATION = 'real_estate_portal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('PGDATABASE'),
-        'USER': os.getenv('PGUSER'),
-        'PASSWORD': os.getenv('PGPASSWORD'),
-        'HOST': os.getenv('PGHOST'),
-        'PORT': os.getenv('PGPORT'),
+        'NAME': os.getenv('PGDATABASE', 'railway'),  # 'railway' — значение по умолчанию
+        'USER': os.getenv('PGUSER', 'postgres'),
+        'PASSWORD': os.getenv('PGPASSWORD', 'qwUAvTjFTpNyQfCEZCSVqbxTecEJRHCP'),
+        'HOST': os.getenv('PGHOST', 'trolley.proxy.rlwy.net'),
+        'PORT': os.getenv('PGPORT', '15306'),
     }
 }
 
