214e5270068af937473c21ffdb2d0e836b0867d0
wicsion <goldin301@gmail.com>
2025-06-05 02:16:20 +0300

    c

--- real_estate_portal/settings.py
+++ real_estate_portal/settings.py
@@ -98,9 +98,14 @@ WSGI_APPLICATION = 'real_estate_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',     }
- }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
+    }
+}
 
 # Валидация паролей
 AUTH_PASSWORD_VALIDATORS = [
