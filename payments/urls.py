6f525ff90fd0d6f2f829fa08e106defbd33ff78d
wicsion <goldin301@gmail.com>
2025-06-13 03:48:07 +0300

    c

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import payment_topup_view, payment_success_view, yookassa_webhook
+from .views import payment_topup_view, payment_success_view,
 
 
 urlpatterns = [
     path('payment/topup/', payment_topup_view, name='payment_topup'),
     path('payment/success/', payment_success_view, name='payment_success'),
-    path('yookassa-webhook/', yookassa_webhook, name='yookassa_webhook'),
 ]
